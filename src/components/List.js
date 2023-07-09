033c1a25004063b9b90f0dd7743826e799b44d77
unknown <khushalyadav2811@gmail.com>
2023-07-09 15:26:22 +0530

    first build

---
+++ src/components/List.js
@@ -0,0 +1,35 @@
+
+const List = () =>{
+    return (
+        <div className="list">
+      <div className="row">
+        <h2 className="text-white title">Netflix</h2>
+        <div className="col">
+          <div className="row__posters">
+            <img className="row__poster row__posterLarge"
+                src="https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg"
+                alt=""
+                />
+            <img className="row__poster row__posterLarge"
+                src="https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg"
+                alt=""
+                />
+            <img className="row__poster row__posterLarge"
+                src="https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg"
+                alt=""
+                />
+            <img className="row__poster row__posterLarge"
+                src="https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg"
+                alt=""
+                />
+            <img className="row__poster row__posterLarge"
+                src="https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg"
+                alt=""
+                />
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+export default List;
---
+++ src/components/firebaseCon.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDL7FZl9aeXOALSNI-E5yNUz0MSNkZafRs",
+  authDomain: "streamx-7f680.firebaseapp.com",
+  projectId: "streamx-7f680",
+  storageBucket: "streamx-7f680.appspot.com",
+  messagingSenderId: "490892790520",
+  appId: "1:490892790520:web:6a23803f1cdc26caa8c90d",
+  measurementId: "G-PLQZ23NBXN"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
